824b8790a8d2dd1702b0853187f1f28e7256ece5
Tural Ari <tural.ari@etu.univ-lyon1.fr>
2025-09-25 08:48:32 +0200

    exercice 3.1

--- src/controllers/book.controller.ts
+++ src/controllers/book.controller.ts
@@ -1,12 +1,28 @@
 import { Controller, Get, Route, Tags } from "tsoa";
 import { BookDTO } from "../dto/book.dto";
 import { bookService } from "../services/book.service";
+import { CustomError } from "../middlewares/errorHandler";
+import { Book } from "../models/book.model";
+import { toDto } from "../mapper/book.mapper";
 
 @Route("books")
 @Tags("Books")
 export class BookController extends Controller {
-  @Get("/")
-  public async getAllBooks(): Promise<BookDTO[]> {
-    return bookService.getAllBooks();
-  }
+    @Get("/")
+    public async getAllBooks(): Promise<BookDTO[]> {
+        return bookService.getAllBooks();
+    }
+
+    @Get("{id}")
+    public async getBoodById(id: number): Promise<BookDTO> {
+        let book: Book | null = await bookService.getBookById(id);
+
+        if(book === null) {
+            let error: CustomError = new Error(`Book ${id} not found`);
+            error.status = 404;
+            throw error;
+        }
+
+        return toDto(book);
+    }
 }
--- src/services/book.service.ts
+++ src/services/book.service.ts
@@ -2,14 +2,25 @@ import { Author } from "../models/author.model";
 import { Book } from "../models/book.model";
 
 export class BookService {
-  public async getAllBooks(): Promise<Book[]> {
-    return Book.findAll({
-        include: [{
-            model: Author,
-            as: 'author'
-        }]
-    });
-  }
+    public async getAllBooks(): Promise<Book[]> {
+        return Book.findAll({
+            include: [{
+                model: Author,
+                as: 'author'
+            }]
+        });
+    }
+
+    public async getBookById(id: number): Promise<Book | null > {
+        return Book.findByPk(id, {
+            include: [
+                {
+                    model: Author,
+                    as: 'author',
+                },
+            ],
+        });
+    }
 }
 
 export const bookService = new BookService();
