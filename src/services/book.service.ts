1500b6fd0687a2b83ddd6f50e03da968098568a4
Tural Ari <tural.ari@etu.univ-lyon1.fr>
2025-09-25 09:59:09 +0200

    exercice 3.2

--- src/services/book.service.ts
+++ src/services/book.service.ts
@@ -1,7 +1,13 @@
 import { Author } from "../models/author.model";
 import { Book } from "../models/book.model";
+import {BookDTO} from "../dto/book.dto";
+import {AuthorService} from "./author.service";
+import {CustomError} from "../middlewares/errorHandler";
 
 export class BookService {
+
+    public authorService = new AuthorService();
+
     public async getAllBooks(): Promise<Book[]> {
         return Book.findAll({
             include: [{
@@ -9,6 +15,21 @@ export class BookService {
                 as: 'author'
             }]
         });
+    }
+
+    public async createBook(
+        title: string,
+        publishYear: number,
+        authorId: number,
+        isbn: string,
+    ): Promise<Book> {
+        let author: Author | null = await this.authorService.getAuthorById(authorId);
+        if (!author) {
+            let error: CustomError = new Error("Author not found");
+            error.status = 404;
+            throw error;
+        }
+        return Book.create({ title, publishYear, authorId, isbn });
     }
 
     public async getBookById(id: number): Promise<Book | null > {
